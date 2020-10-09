661fa4a4fa9b14bd380c3bd3e8c1e378282c4d04
Brett Porter <brett.porter@codeandtheory.com>
2020-10-09 11:11:03 -0700

    initial

--- aws/dynamo-table-tf-state/main.tf
+++ aws/dynamo-table-tf-state/main.tf
@@ -1,7 +1,7 @@
 # Create a dynamodb table for locking a Terraform state file
 
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "${var.app}-tf-state-lock"
+  name = "${var.client}-${var.app}-${var.env}-tf-state-lock"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
@@ -12,7 +12,7 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   }
 
   tags = {
-    Name = "${var.app} DynamoDB TF State Lock Table"
+    Name = "${var.client}-${var.app}-${var.env} DynamoDB TF State Lock Table"
     Client = var.client
     Env = var.env
   }
