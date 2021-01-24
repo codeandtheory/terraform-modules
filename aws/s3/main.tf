eb65bc6cb07c40c026893fbe1c463da9f5bebe27
Kjartan Clausen <kjartan.clausen@codeandtheory.com>
2021-01-24 22:18:28 +0100

    upgrade

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -9,34 +9,14 @@ resource "aws_s3_bucket" "bucket" {
   tags = merge(var.tags, map("Name", format("%s", var.s3_name)))
 }
 
-#resource "aws_s3_bucket_policy" "private" {
-#  count  = var.allow_public != true ? 1 : 0
-#  bucket = aws_s3_bucket.bucket.id
-#
-#  policy = <<EOF
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Sid": "",
-#      "Action": ["s3:*"],
-#      "Effect": "Allow",
-#      "Resource": ["arn:aws:s3:::${var.s3_name}",
-#                   "arn:aws:s3:::${var.s3_name}/*"],
-#      "Principal": {
-#        "AWS": ["arn:aws:iam::${var.aws_account_id}:user/${var.aws_username}"]
-#      }
-#    }
-#  ]
-#}
-#EOF
-#}
 resource "aws_s3_bucket_public_access_block" "private" {
   count  = var.allow_public != true ? 1 : 0
   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "public" {
