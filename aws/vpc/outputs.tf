42c049ea6e7643ac6d2fb56a19cbb53b23700061
Kjartan Clausen <kjartan.clausen@codeandtheory.com>
2021-01-25 10:05:05 +0100

    bugfix

--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -14,10 +14,10 @@ output "rds_subnet_ids" {
   value = aws_subnet.elb_subnet.*.id
 }
 
-#output "rds_subnet_group" {
-#  value = aws_db_subnet_group.rds_subnet_group[0].id
-#}
-#
-#output "rds_subnet_group_name" {
-#  value = aws_db_subnet_group.rds_subnet_group[0].name
-#}
+output "rds_subnet_group" {
+  value = aws_db_subnet_group.rds_subnet_group[0].id
+}
+
+output "rds_subnet_group_name" {
+  value = aws_db_subnet_group.rds_subnet_group[0].name
+}
