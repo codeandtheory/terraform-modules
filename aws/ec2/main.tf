abfdf3df3e962cee9bb9b2597b12bb254e74650f
Kjartan Clausen <kjartan.clausen@codeandtheory.com>
2021-02-09 13:17:26 -0500

    added EIP assignment to the EC2 module

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -2,53 +2,63 @@
 data "aws_ami" "centos" {
   owners      = ["679593333241"]
   most_recent = true
-
   filter {
     name   = "name"
     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
-
   filter {
     name   = "architecture"
     values = ["x86_64"]
   }
-
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
 }
 
+# Get the latest Ubuntu 20.04 LTS AMI
 data "aws_ami" "ubuntu" {
     owners = ["099720109477"] # Canonical
     most_recent = true
-
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
     }
-
     filter {
         name   = "virtualization-type"
         values = ["hvm"]
     }
-
 }
 
+# Create the EC2 instance
 resource "aws_instance" "ec2_instance" {
-  count = var.instance_count
-  ami = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  key_name = var.key_pair
-  subnet_id = var.subnet_ids[(count.index % length(var.subnet_ids))]
-  associate_public_ip_address = false
-  vpc_security_group_ids = var.vpc_security_group_ids
+  count                       = var.instance_count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair
+  subnet_id                   = var.subnet_ids[(count.index % length(var.subnet_ids))]
+  associate_public_ip_address = var.associate_public_ip_address
+  vpc_security_group_ids      = var.vpc_security_group_ids
   
   tags = {
-    Name    = "${var.client}-${var.app}-${var.env}-${var.server_type}-${count.index + 1}"
-    Env     = var.env
-    App     = var.app
-    Client  = var.client
+    Name        = "${var.client}-${var.app}-${var.env}-${var.server_type}-${count.index + 1}"
+    Env         = var.env
+    App         = var.app
+    Client      = var.client
+    "Tech Lead" = var.techlead
+  }
+}
+
+# Assign EIP if var.associate_public_ip_address and var.assign_eip_address are "true"
+resource "aws_eip" "default" {
+  count             = var.associate_public_ip_address && var.assign_eip_address ? 1 : 0
+  network_interface = join("", aws_instance.ec2_instance.*.primary_network_interface_id)
+  vpc               = true
+  tags = {
+    Name        = "${var.client}-${var.app}-${var.env}-${var.server_type}-${count.index + 1}"
+    Env         = var.env
+    App         = var.app
+    Client      = var.client
     "Tech Lead" = var.techlead
   }
 }
--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -3,43 +3,64 @@
 #----------------------------------------------------------------
 
 variable "app" {
+  type        = string
 }
 
 variable "env" {
+  type        = string
 }
 
 variable "client" {
+  type        = string
 }
 
-  
 variable "techlead" {
   description = "Tech Lead"
+  type        = string
   default     = "John Doe"
 }
 
 variable "instance_count" {
   description = "Number of instances to spin up"
-  default = "2"
+  type        = number
+  default     = "2"
 }
 
 variable "instance_type" {
   description = "Size of the EC2 instances"
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_pair" {
   description = "ssh key to be used initially"
-  default = "devops"
+  type        = string
+  default     = "devops"
 }
 
 variable "server_type" {
-  description = "The type of server being spun up"
-  default = "www"
+  description = "The type of server being spun up (web, api, util, ...)"
+  default     = "www"
+  type        = string
 }
 
 variable "subnet_ids" {
 }
 
 variable "vpc_security_group_ids" {
-type    = list(string)
+  description = "List of Security Group IDs allowed to connect to the instance"
+  type        = list(string)
+  default     = []
+}
+
+variable "associate_public_ip_address" {
+  type        = bool
+  description = "Associate a public IP address with the instance"
+  default     = false
+}
+
+variable "assign_eip_address" {
+  type        = bool
+  description = "Assign an Elastic IP address to the instance"
+  default     = true
 }
