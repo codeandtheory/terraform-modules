2da5d932a2f71fb903198bae30fec8125ece2a89
Kjartan Clausen <kjartan.clausen@codeandtheory.com>
2021-02-22 17:45:21 -0500

    Added the github_webhook module from the devops repo. Deleted Brett's security_groups module

---
+++ aws/github_webhook/main.tf
@@ -0,0 +1,21 @@
+provider "github" {
+  token        = "${var.github_token}"
+  organization = "${var.github_organization}"
+}
+
+data "github_repository" "repo" {
+  full_name = var.repo
+}
+
+resource "github_repository_webhook" "hook" {
+  repository = data.github_repository.repo.name
+
+  configuration {
+    url          = var.url
+    content_type = "json"
+    insecure_ssl = false
+    secret       = var.secret
+  }
+  active = true
+  events = var.events
+}
