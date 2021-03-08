b33e7529b2a3a80c2bb2c563b2654c29ee813325
Kjartan Clausen <69314295+kclausen-ct@users.noreply.github.com>
2021-03-08 17:16:09 -0500

    Hashicorp updated their "terraform-aws-modules/rds/aws" so we've gotta pick a specific version

--- aws/mysql/main.tf
+++ aws/mysql/main.tf
@@ -47,7 +47,7 @@ resource "aws_kms_key" "api_keys" {
 
 module "database" {
   source  = "terraform-aws-modules/rds/aws"
-  version = "~> 2.0"
+  version = "2.23.0"
 
   identifier = var.db_identifier
 
